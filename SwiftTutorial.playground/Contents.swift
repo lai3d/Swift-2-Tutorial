15804547af4996e0b8df7f97d747317f665d7a4e
Larry Lai <laiyanlin@gmail.com>
2016-07-12 16:59:09 +0800

    Arrays and For Loops

--- SwiftTutorial.playground/Contents.swift
+++ SwiftTutorial.playground/Contents.swift
@@ -60,3 +60,38 @@ tipCalc.printPossibleTips()
 
 // Arrays and For Loops
 
+// 1
+class TipCalculator2 {
+    // 2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    // 3
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    // 4
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    // 5
+    func printPossibleTips() {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        for possibleTip in possibleTipsInferred {
+            print("\(possibleTip*100)%: \(calcTipWithTipPct(possibleTip))")
+        }
+    }
+}
+
+// 6
+let tipCalc2 = TipCalculator2(total: 33.25, taxPct: 0.06)
+tipCalc2.printPossibleTips()
+
+
