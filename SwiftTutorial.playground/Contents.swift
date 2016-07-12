0f053710da4a49ce2d0aa4a8108732a078842084
Larry Lai <laiyanlin@gmail.com>
2016-07-12 16:41:54 +0800

    Classes and Methods

--- SwiftTutorial.playground/Contents.swift
+++ SwiftTutorial.playground/Contents.swift
@@ -1,5 +1,8 @@
 //: Playground - noun: a place where people can play
 
+
+// Basic Types and Control Flow in Swift
+
 let swiftTeam = 13
 let iOSTeam = 54
 let otherTeams = 48
@@ -22,3 +25,35 @@ if onSaleInferred {
     print("\(nameInferred) at regular price: \(priceInferred)!")
 }
 
+// Classes and Methods
+
+// 1
+class TipCalculator {
+    // 2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    // 3
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    // 4
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    // 5
+    func printPossibleTips() {
+        print("15%: \(calcTipWithTipPct(0.15))")
+        print("18%: \(calcTipWithTipPct(0.18))")
+        print("20%: \(calcTipWithTipPct(0.20))")
+    }
+}
+
+// 6
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.printPossibleTips()
