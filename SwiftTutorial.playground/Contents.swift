b18378e6c24d15acb7e29b10cb2dae2bf63e92be
Larry Lai <laiyanlin@gmail.com>
2016-07-12 17:25:29 +0800

    Dictionaries

--- SwiftTutorial.playground/Contents.swift
+++ SwiftTutorial.playground/Contents.swift
@@ -60,6 +60,8 @@ tipCalc.printPossibleTips()
 
 // Arrays and For Loops
 
+print("\nArrays and For Loops\n")
+
 // 1
 class TipCalculator2 {
     // 2
@@ -82,7 +84,7 @@ class TipCalculator2 {
     // 5
     func printPossibleTips() {
         let possibleTipsInferred = [0.15, 0.18, 0.20]
-        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        let _:[Double] = [0.15, 0.18, 0.20]
         
         for possibleTip in possibleTipsInferred {
             print("\(possibleTip*100)%: \(calcTipWithTipPct(possibleTip))")
@@ -93,5 +95,43 @@ class TipCalculator2 {
 // 6
 let tipCalc2 = TipCalculator2(total: 33.25, taxPct: 0.06)
 tipCalc2.printPossibleTips()
+
+// Dictionaries
+
+print("Dictionaries")
+
+class TipCalculator3 {
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    // 1
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        // 2
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            // 3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
+
+let tipCalc3 = TipCalculator3(total: 33.25, taxPct: 0.06)
+tipCalc3.returnPossibleTips()
 
 
