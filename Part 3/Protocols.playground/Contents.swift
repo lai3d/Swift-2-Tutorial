f1edf39f1c71d054779d7af970addabc931b217b
Larry Lai <laiyanlin@gmail.com>
2016-07-15 18:02:54 +0800

    Optional Protocols

---
+++ Part 3/Protocols.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+@objc protocol Speaker {
+    func Speak()
+    optional func TellJoke()
+}
+
+class Vicki: Speaker {
+    @objc func Speak() {
+        print("Hello, I am Vicki!")
+    }
+    @objc func TellJoke() {
+        print("Q: What did Sushi A say to Sushi B?")
+    }
+}
+
+class Ray: Speaker {
+    @objc func Speak() {
+        print("Yo, I am Ray!")
+    }
+    @objc func TellJoke() {
+        print("Q: Whats the object-oriented way to become wealthy?")
+    }
+    func WriteTutorial() {
+        print("I'm on it!")
+    }
+}
+
+class Animal {
+}
+    
+class Dog : Animal, Speaker {
+    @objc func Speak() {
+        print("Woof!")
+    }
+}
