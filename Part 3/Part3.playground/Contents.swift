93134b52a54891260266795f585ff1a768a9404a
Larry Lai <laiyanlin@gmail.com>
2016-07-15 16:59:12 +0800

    A Full Prototype

--- Part 3/Part3.playground/Contents.swift
+++ Part 3/Part3.playground/Contents.swift
@@ -1,3 +1,40 @@
 //: Playground - noun: a place where people can play
 
-
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+//    func calcTipWithTipPct(tipPct: Double) -> Double {
+//        return subtotal * tipPct
+//    }
+    
+    func calcTipWithTipPct(tipPct:Double) -> (tipAmt:Double, total:Double) {
+        let tipAmt = subtotal * tipPct
+        let finalTotal = total + tipAmt
+        return (tipAmt, finalTotal)
+    }
+    
+    func returnPossibleTips() -> [Int: (tipAmt:Double, total:Double)] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: (tipAmt:Double, total:Double)]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+    
+}
